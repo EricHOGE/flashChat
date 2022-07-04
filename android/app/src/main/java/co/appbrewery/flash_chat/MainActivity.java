628d79e6d7a418add998049f2310119bd3280715
Eric HOGEDEZ <erichogedez2@gmail.com>
2022-07-04 14:35:04 +0200

    first commit

---
+++ android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
@@ -0,0 +1,10 @@
+/* package com.ericH.flash_chat; */
+package com.ericH.flash_chat;
+
+
+import io.flutter.embedding.android.FlutterActivity;
+
+
+public class MainActivity extends FlutterActivity {
+  
+}
